e6309815981e649dbc6f6d185a7fae1166eaadb5
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:23:09 +0300

    Initial commit

---
+++ BASAMAK AYIRMA/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace BASAMAK_AYIRMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(95, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sayıyı Yazınız";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(320, 60);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Basamak Ayırma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BASAMAK AYIRMA/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BASAMAK_AYIRMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Değişkenler
+
+        int x1=0,x2=0,x3=3;
+
+        string k = ".";
+
+        string f;
+
+        #endregion
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            foreach (char b in textBox1.Text)
+            {
+                x1++;
+                x2++;
+            }
+
+            if (x1 % x3 == 0)
+            {
+                x1 = 0;
+
+                x3 = x3 + 4;
+                f = textBox1.Text.Insert(x2, k);
+                textBox1.Text = f;
+
+
+               textBox1.SelectionStart = textBox1.Text.Length;
+               textBox1.Focus();
+
+               // yukarıdaki iki satırlık kodun yaptığı işlemi tek başına aşağıdaki
+               // açıklama satırı içindeki kodda yapabilir. amacımız imleci textboxın son 
+               // karakterine focuslamak.
+
+               //SendKeys.Send("^{END}"); 
+                
+                x2= 0;
+
+            }
+               
+            else
+            {
+                x1 = 0;
+                x2 = 0;
+            }
+             
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
